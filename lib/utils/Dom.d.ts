ba15b3652307bf99bf40c939d192d5b74bbe066d
Daniel Imms <daimms@microsoft.com>
2018-01-22 06:34:20 -0800

    Build lib

--- lib/utils/Dom.d.ts
+++ lib/utils/Dom.d.ts
@@ -1,2 +1,2 @@
-import { IDisposable } from "../Interfaces";
+import { IDisposable } from '../Interfaces';
 export declare function addDisposableListener(node: Element | Window | Document, type: string, handler: (e: any) => void, useCapture?: boolean): IDisposable;
