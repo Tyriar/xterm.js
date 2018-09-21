d4c89485d723c0d52264924a048f9f3592559c67
Daniel Imms <daimms@microsoft.com>
2018-09-21 11:53:08 -0700

    Improve scripts, don't build attach addon in src build

--- src/public/Terminal.test.ts
+++ src/public/Terminal.test.ts
@@ -5,11 +5,10 @@
 
 import { assert } from 'chai';
 import { Terminal } from './Terminal';
-import * as attach from '../addons/attach/attach';
 
 describe('Terminal', () => {
   it('should apply addons with Terminal.applyAddon', () => {
-    Terminal.applyAddon(attach);
+    Terminal.applyAddon(require('../../../lib/addons/attach/attach'));
     // Test that addon was applied successfully, adding attach to Terminal's
     // prototype.
     assert.equal(typeof (<any>Terminal).prototype.attach, 'function');
